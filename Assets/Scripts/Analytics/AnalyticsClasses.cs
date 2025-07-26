6f32a451b9800f593c9d130616bfce354d1953af
Soumyadip Pal <arka.pal21@gmail.com>
2025-07-26 19:37:19 +0530

    Added analytics classes

---
+++ Assets/Scripts/Analytics/AnalyticsClasses.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Analytics
+{
+    [System.Serializable]
+    public class ForkliftData
+    {
+        public Module module;
+        public GameData gameData;
+    }
+
+    [System.Serializable]
+    public class Module
+    {
+        public string moduleName;
+        public string moduleType;
+        public string moduleVersion;
+        public string moduleDescription;
+    }
+
+    [System.Serializable]
+    public class GameData
+    {
+        public string startTime;
+        public string endTime;
+        public float duration;
+    } 
+}
+
